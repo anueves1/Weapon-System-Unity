c8e21f90308a62655ad6cc41f1650c745abdebfc
anueves1 <34652784+ProfStudGit@users.noreply.github.com>
2018-10-29 02:29:18 +0100

    abc

---
+++ Assets/Plugins/WP-Pistol3Pack/IronbellyWeapons_ReadMe_Unity.txt
@@ -0,0 +1,33 @@
+Thank you for your purchase, hopefully you find some great use for your new weapon(s)!  If you have any questions or need any customization done, please feel free to reach out to us at support@ironbellystudios.com or check out our support thread here:  http://forum.unity3d.com/threads/aaa-quality-fps-pistol-asset-pack-from-ironbelly-studios.362469/
+
+OPEN PACKAGE
+Open the .unitypackage file in Unity.  You should have version 5.1.2 or higher installed.
+
+ANIMATIONS
+To demo the animations for all 3 pistols, follow the instructions below. Please be advised that this prefab and accompanying script is for demo purposes only.
+1) In the Project window, select the Assets folder
+2) Drag the Pistol3Pack_prefab into your scene
+3) Select the prefab in your Hierarchy window, then set the position to (0,0,0) in the Inspector window
+4) Select the camera in your Hierachy window, then set the Projection dropdown to Perspective
+4) Click the play button
+5) View the Game window and click any animation button to preview animations. Click the TT, M9, and Mag44 buttons to switch pistols. Click the STOP button to stop all animation.
+6) Enjoy!
+
+Alloy-ONLY MATERIALS
+There are materials in this pack that require the Alloy Shader:
+/Assets/WP-Pistol3Pack/WP-44Mag/Meshes/Materials/FPArms_DIF_ALLOYONLY
+/Assets/WP-Pistol3Pack/WP-44Mag/Meshes/Materials/44Mag_COL_ALLOYONLY
+/Assets/WP-Pistol3Pack/WP-M9/Meshes/Materials/M9_COL_ALLOYONLY
+/Assets/WP-Pistol3Pack/WP-TT/Meshes/Materials/TT_COL_ALLOYONLY
+/Assets/WP-Pistol3Pack/WP-TT/Meshes/Materials/FPArms_Male1_COL_ALLOYONLY
+
+For all users that do not have Alloy installed, these materials will appear a bright magenta and show Hidden/InternalErrorShader in the Inspector window.  In order to make use of these materials you need to purchase and install the Alloy Physical Shader Framework by heading over to https://www.assetstore.unity3d.com/en/#!/content/11978 and importing it into your project.  
+
+If you do not have Alloy, worry not, we have provided standard Unity 5 PBR materials as well that can be found in the same location and are missing the _ALLOYONLY tag on the end of the material name:
+/Assets/WP-Pistol3Pack/WP-44Mag/Meshes/Materials/FPArms_DIF
+/Assets/WP-Pistol3Pack/WP-44Mag/Meshes/Materials/44Mag_COL
+/Assets/WP-Pistol3Pack/WP-M9/Meshes/Materials/M9_COL
+/Assets/WP-Pistol3Pack/WP-TT/Meshes/Materials/TT_COL
+/Assets/WP-Pistol3Pack/WP-TT/Meshes/Materials/FPArms_Male1_COL
+
+These materials look awesome as well and will work perfectly with a stock Unity install.
---
+++ Assets/Plugins/WP-Pistol3Pack/playAllAnimations.cs
@@ -0,0 +1,165 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playAllAnimations : MonoBehaviour {
+	//it'll be re-written to use arrays when weapon list grows bigger
+	public GameObject arms1; //1 is m9 here, 2 is tt, 3 is mag44
+	public GameObject arms2;
+	public GameObject arms3;
+	public GameObject weapon1;
+	public GameObject weapon2;
+	public GameObject weapon3;
+	//public bool[] animPlaying;
+	//private int animAmount = 6;
+	//private int playingAnimIndex = -1;
+	public Camera mainCameraGO;
+	public GameObject mainCameraSnapObject1;
+	public GameObject mainCameraSnapObject2;
+	public GameObject mainCameraSnapObject3;
+	public float cameraNormalFov = 60f;
+	public float cameraZoomFov = 24f;
+	public float cameraNearClipping = 0.1f;
+	// Use this for initialization
+	void Start () {
+//		animPlaying = new bool[animAmount];
+//		for (int x=0; x> animAmount; x++)
+//		{
+//
+//			animPlaying[x] = false;
+//
+//		}
+		mainCameraGO = Camera.main;
+		mainCameraSnapObject1 = GameObject.FindGameObjectWithTag("mainCameraSnapObject1");
+		mainCameraSnapObject2 = GameObject.FindGameObjectWithTag("mainCameraSnapObject2");
+		mainCameraSnapObject3 = GameObject.FindGameObjectWithTag("mainCameraSnapObject3");
+		mainCameraGO.transform.position = mainCameraSnapObject1.transform.position;
+		mainCameraGO.transform.rotation = mainCameraSnapObject1.transform.rotation;
+		mainCameraGO.nearClipPlane = cameraNearClipping;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+
+
+
+
+	public void playReload()
+	{
+		arms1.GetComponent<Animation>().Play("M9-Reload");
+		weapon1.GetComponent<Animation>().Play("M9-Reload", PlayMode.StopAll);
+		arms2.GetComponent<Animation>().Play("TT-Reload");
+		weapon2.GetComponent<Animation>().Play("TT-Reload", PlayMode.StopAll);
+		arms3.GetComponent<Animation>().Play("44-Reload");
+		weapon3.GetComponent<Animation>().Play("44-Reload", PlayMode.StopAll);
+
+
+
+		mainCameraGO.fieldOfView = cameraNormalFov;
+	}
+	public void playFire()
+	{
+		arms1.GetComponent<Animation>().Play("M9-Fire");
+		weapon1.GetComponent<Animation>().Play("M9-Fire", PlayMode.StopAll);
+		arms2.GetComponent<Animation>().Play("TT-Fire");
+		weapon2.GetComponent<Animation>().Play("TT-Fire", PlayMode.StopAll);
+		arms3.GetComponent<Animation>().Play("44-Fire");
+		weapon3.GetComponent<Animation>().Play("44-Fire", PlayMode.StopAll);
+
+		mainCameraGO.fieldOfView = cameraNormalFov;
+	}
+
+	public void playIdle()
+	{
+		arms1.GetComponent<Animation>().Play("M9-Idle");
+		weapon1.GetComponent<Animation>().Play("M9-Idle", PlayMode.StopAll);
+		arms2.GetComponent<Animation>().Play("TT-Idle");
+		weapon2.GetComponent<Animation>().Play("TT-Fire", PlayMode.StopAll);
+		weapon2.GetComponent<Animation>().Stop();
+		arms3.GetComponent<Animation>().Play("44-Idle");
+		weapon3.GetComponent<Animation>().Play("44-Idle", PlayMode.StopAll);
+
+		mainCameraGO.fieldOfView = cameraNormalFov;
+	}
+
+	public void playSprint()
+	{
+		arms1.GetComponent<Animation>().Play("M9-Sprint");
+		weapon1.GetComponent<Animation>().Play("M9-Idle", PlayMode.StopAll);
+		arms2.GetComponent<Animation>().Play("TT-Sprint");
+		weapon2.GetComponent<Animation>().Play("TT-Fire", PlayMode.StopAll);
+		weapon2.GetComponent<Animation>().Stop();
+		arms3.GetComponent<Animation>().Play("44-Sprint");
+		weapon3.GetComponent<Animation>().Play("44-Idle", PlayMode.StopAll);
+
+		mainCameraGO.fieldOfView = cameraNormalFov;
+	}
+
+	public void playADSFire()
+	{
+		arms1.GetComponent<Animation>().Play("M9-ADS-Fire");
+		weapon1.GetComponent<Animation>().Play("M9-Fire", PlayMode.StopAll);
+		arms2.GetComponent<Animation>().Play("TT-ADS-Fire");
+		weapon2.GetComponent<Animation>().Play("TT-Fire", PlayMode.StopAll);
+		arms3.GetComponent<Animation>().Play("44-ADS-Fire");
+		weapon3.GetComponent<Animation>().Play("44-Fire", PlayMode.StopAll);
+
+		mainCameraGO.fieldOfView = cameraZoomFov;
+	}
+	public void playADSIdle()
+	{
+		arms1.GetComponent<Animation>().Play("M9-ADS-Idle");
+		weapon1.GetComponent<Animation>().Play("M9-Idle", PlayMode.StopAll);
+		arms2.GetComponent<Animation>().Play("TT-ADS-Idle");
+		weapon2.GetComponent<Animation>().Play("TT-Fire", PlayMode.StopAll);
+		weapon2.GetComponent<Animation>().Stop();
+		arms3.GetComponent<Animation>().Play("44-ADS-Idle");
+		weapon3.GetComponent<Animation>().Play("44-Idle", PlayMode.StopAll);
+
+		mainCameraGO.fieldOfView = cameraZoomFov;
+	}
+
+	public void playNothing()
+	{
+		arms1.GetComponent<Animation>().Stop();
+		weapon1.GetComponent<Animation>().Stop();
+		arms2.GetComponent<Animation>().Stop();
+		weapon2.GetComponent<Animation>().Stop();
+		arms3.GetComponent<Animation>().Stop();
+		weapon3.GetComponent<Animation>().Stop();
+
+
+		//mainCameraGO.fieldOfView = cameraNormalFov;
+
+	}
+
+	public void switch1()
+	{
+		mainCameraGO.transform.position = mainCameraSnapObject1.transform.position;
+		mainCameraGO.transform.rotation = mainCameraSnapObject1.transform.rotation;
+
+
+	}
+
+	public void switch2()
+	{
+		mainCameraGO.transform.position = mainCameraSnapObject2.transform.position;
+		mainCameraGO.transform.rotation = mainCameraSnapObject2.transform.rotation;
+		
+		
+	}
+
+	public void switch3()
+	{
+		mainCameraGO.transform.position = mainCameraSnapObject3.transform.position;
+		mainCameraGO.transform.rotation = mainCameraSnapObject3.transform.rotation;
+		
+		
+	}
+
+	}
+
